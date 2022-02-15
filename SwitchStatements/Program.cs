3054ef080bbf167abd4f39592d66520d2451171f
Maddi Wade <Madelinenwade@gmail.com>
2022-02-15 10:16:24 -0500

    Add project files.

---
+++ SwitchStatements/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SwitchStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
