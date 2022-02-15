566fe9d6b64c1fb01f8a1a1670e77dab89cfb535
Maddi Wade <Madelinenwade@gmail.com>
2022-02-15 10:52:50 -0500

    Finished!

--- SwitchStatements/Program.cs
+++ SwitchStatements/Program.cs
@@ -6,7 +6,38 @@ namespace SwitchStatements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            Console.WriteLine("what is your favorite class?");
+            var favClass = (Console.ReadLine());
+            switch (favClass)
+            {
+                case "Math":
+                    Console.WriteLine("Oh i hate math"); 
+                    break;
+                case "English":
+                    Console.WriteLine("I bet you love books");
+                    break;
+                case "Science":
+                    Console.WriteLine("Thats also my favorite!");
+                    break;
+                case "History":
+                    Console.WriteLine("Thats a great one!");
+                    break;
+                case "Gym":
+                    Console.WriteLine("Nice, being active is great");
+                    break;
+                default:
+                    Console.WriteLine("Oh weird i have not heard of that");
+                    break;
+
+
+
+
+
+
+            }
+        } 
+    
     }
-}
+}  
+
+
